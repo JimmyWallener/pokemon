62c32cc7b5c90d829e7932579f5f74d09b683b45
Jimmy Wallener <jimmy.wallener@protonmail.com>
2019-11-14 17:37:06 +0100

    added some lines
    changed in .json and added getJSON for datafile.

--- js/script.js
+++ js/script.js
@@ -2,38 +2,45 @@ $(function () {
 
     const pokemonURL = "https://pokeapi.co/api/v2/generation/1";
     const pokemonByName = "https://pokeapi.co/api/v2/pokemon/";
+    const pokemonImage = "data/data.json"
 
     //Min lösning till autocomplete- ni får gärna ta bort det om ni hittar bättre eller smidigare lösning
     var nameArray = [];
-    $.getJSON(pokemonURL, function(data) {
-    $.each(data.pokemon_species, function(index, pokemon) {
-       if ($.inArray(pokemon.name, nameArray) === -1) {
-            nameArray.push(pokemon.name)
-        }
-    })
-    }); 
-        $("#Seek").autocomplete({
-        source: nameArray
+    $.getJSON(pokemonURL, function (data) {
+        $.each(data.pokemon_species, function (index, pokemon) {
+            if ($.inArray(pokemon.name, nameArray) === -1) {
+                nameArray.push(pokemon.name)
+            }
+        })
     });
-          
+    $("#Seek").autocomplete({
+        source: nameArray,
+        autoFocus: true,
+        select: function (event, ui) {
+            event.preventDefault();
+            $("#Seek").val(ui.item.label);
+            return false;
+        }
+    });
+
     // Eventlisteners go here
 
     $(document).ready(function () {
         $.getJSON(pokemonURL).done(function (data) {
             $.each(data.pokemon_species, function (index, pokemon) {
                 let name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-                // autocomp(pokemon); <-- callback till autocomplete
-
                 $("#getmyPoke").click(function (event) {
                     event.preventDefault();
 
                     $.getJSON(pokemonByName + pokemon.name).done(function (details) {
-                        let userInput = $("#Seek").val();
+                        $.getJSON(pokemonImage).done(function (image) {
+                            let userInput = $("#Seek").val();
+                            console.log(image);
 
-                        if (userInput.toLowerCase() == name.toLowerCase()) {
-                           
-                            getPokemon(details);
-                        }
+                            if (userInput.toLowerCase() == name.toLowerCase()) {
+                                getPokemon(details, image);
+                            }
+                        })
                     })
                     $("#clear").click(function (event) {
                         event.preventDefault();
@@ -57,30 +64,20 @@ $(function () {
 function getPokemon(details) {
     let detail = []; // Saves details data in javascript objet (since its a prerequisite for the assignment)
     let $pokemonDiv = $("#pokemon-details");
-    
+
     $pokemonDiv.empty();
     $("#Seek").val('');
     detail = details;
     let moves = detail.abilities;
 
-    $pokemonDiv.append(`<h2>${details.name}</h2>`);  
-    //pokemonDiv.append("<img src = ' "+ details.sprites.front_default + "'>")
-    //pokemonDiv.append("<img src = ' "+ details.sprites.back_default + "'>")
-    $pokemonDiv.append(`<img src = '${detail.sprites.front_shiny}'>`);
-    $pokemonDiv.append(`<img src = '${detail.sprites.back_shiny}'>`);
+    $pokemonDiv.append(`<h2>${details.name}</h2>`);
+    //pokemonDiv.append("</img src = ' "+ details.sprites.front_default + "'>")
+    //pokemonDiv.append("</img src = ' "+ details.sprites.back_default + "'>")
+    $pokemonDiv.append(`</img src = '${detail.sprites.front_shiny}'>`);
+    $pokemonDiv.append(`</img src = '${detail.sprites.back_shiny}'>`);
     for (let i = 0; i < moves.length; i++) {
         let obj = moves[i].ability;
         $pokemonDiv.append(`<p>Abilities: ${obj.name}</p>`);
     }
 }
-// Autocomplete function - fungerar ej ännu.
-/*
-function autocomp(pokemon) {
-    let auto = [];
-    auto = pokemon;
-    $("#Seek").autocomplete({
-        minlength: 0,
-        source: auto
-    }); console.log(auto);
-}
-*/
+
